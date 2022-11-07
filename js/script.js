0e28ef26508f69364b5b91b0452bf20a2534e2c5
Alexandr <alexander@burlachenko>
2022-11-07 21:15:52 +0200

    adds boom sound and finishes planned test project

--- js/script.js
+++ js/script.js
@@ -20,47 +20,57 @@ class Explosion {
     this.width = this.spriteWidth * 0.7;
     this.height = this.spriteHeight * 0.7;
     // multiplication is faster than division CPU wise
-    this.x = x - this.width / 2;
-    this.y = y - this.height / 2;
+    this.x = x; //- this.width / 2;
+    this.y = y; //- this.height / 2;
     this.image = new Image();
     this.image.src = "./pictures/boom.png";
     this.frame = 0;
     this.timer = 0;
+    this.angle = Math.random() * 6.2;
+    this.sound = new Audio();
+    this.sound.src = "8bit_bomb_explosion.wav";
   }
   update() {
+    if (this.frame === 0) this.sound.play();
     ++this.timer;
-    if (this.timer % 20 === 0) {
+    if (this.timer % 10 === 0) {
       ++this.frame;
     }
   }
   draw() {
+    ctx.save(); // save current state of canvas to make sure
+    //that following change will effect only one draw call
+    ctx.translate(this.x, this.y); // translate rotation center point
+    ctx.rotate(this.angle); // rotate canvas context
+
     ctx.drawImage(
       this.image,
       this.spriteWidth * this.frame,
       0,
       this.spriteWidth,
       this.spriteHeight,
-      this.x,
-      this.y,
+      0 - this.width / 2, // this.x,
+      0 - this.height / 2, // this.y,
       this.width,
       this.height
     );
+
+    ctx.restore(); // restore original save point
   }
 }
 
 window.addEventListener("click", function (e) {
   createAnimation(e);
 });
-window.addEventListener("mousemove", function (e) {
-  createAnimation(e);
-});
+// window.addEventListener("mousemove", function (e) {
+//   createAnimation(e);
+// });
 
 function createAnimation(e) {
   let positionX = e.x - canvasPosition.left;
   let positionY = e.y - canvasPosition.top;
 
   explosions.push(new Explosion(positionX, positionY));
-  console.log(explosions);
 }
 
 function animate() {
